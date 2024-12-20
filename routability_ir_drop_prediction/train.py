29cc1cb47d283831433be6f317ef4eda90c317df
wwwwwly <2462387506@qq.com>
2024-12-20 19:37:35 +0800

    add trainging log file

--- routability_ir_drop_prediction/train.py
+++ routability_ir_drop_prediction/train.py
@@ -12,24 +12,21 @@ from models.build_model import build_model
 from utils.configs import Parser
 from math import cos, pi
 import sys, os, subprocess
+from pathlib import Path
 
 
 def checkpoint(model, epoch, save_path):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     model_out_path = f"./{save_path}/model_iters_{epoch}.pth"
-    torch.save({'state_dict': model.state_dict()}, model_out_path)
+    torch.save({"state_dict": model.state_dict()}, model_out_path)
     print("Checkpoint saved to {}".format(model_out_path))
-        
 
 
 class CosineRestartLr(object):
-    def __init__(self,
-                 base_lr,
-                 periods,
-                 restart_weights = [1],
-                 min_lr = None,
-                 min_lr_ratio = None):
+    def __init__(
+        self, base_lr, periods, restart_weights=[1], min_lr=None, min_lr_ratio=None
+    ):
         self.periods = periods
         self.min_lr = min_lr
         self.min_lr_ratio = min_lr_ratio
@@ -37,15 +34,14 @@ class CosineRestartLr(object):
         super().__init__()
 
         self.cumulative_periods = [
-            sum(self.periods[0:i + 1]) for i in range(0, len(self.periods))
+            sum(self.periods[0 : i + 1]) for i in range(0, len(self.periods))
         ]
 
         self.base_lr = base_lr
 
-    def annealing_cos(self, start: float,
-                    end: float,
-                    factor: float,
-                    weight: float = 1.) -> float:
+    def annealing_cos(
+        self, start: float, end: float, factor: float, weight: float = 1.0
+    ) -> float:
         cos_out = cos(pi * factor) + 1
         return end + 0.5 * weight * (start - end) * cos_out
 
@@ -53,9 +49,10 @@ class CosineRestartLr(object):
         for i, period in enumerate(cumulative_periods):
             if iteration < period:
                 return i
-        raise ValueError(f'Current iteration {iteration} exceeds '
-                        f'cumulative_periods {cumulative_periods}')
-
+        raise ValueError(
+            f"Current iteration {iteration} exceeds "
+            f"cumulative_periods {cumulative_periods}"
+        )
 
     def get_lr(self, iter_num, base_lr: float):
         target_lr = self.min_lr  # type:ignore
@@ -68,25 +65,24 @@ class CosineRestartLr(object):
         alpha = min((iter_num - nearest_restart) / current_periods, 1)
         return self.annealing_cos(base_lr, target_lr, alpha, current_weight)
 
-    
     def _set_lr(self, optimizer, lr_groups):
         if isinstance(optimizer, dict):
             for k, optim in optimizer.items():
                 for param_group, lr in zip(optim.param_groups, lr_groups[k]):
-                    param_group['lr'] = lr
+                    param_group["lr"] = lr
         else:
-            for param_group, lr in zip(optimizer.param_groups,
-                                        lr_groups):
-                param_group['lr'] = lr
+            for param_group, lr in zip(optimizer.param_groups, lr_groups):
+                param_group["lr"] = lr
 
     def get_regular_lr(self, iter_num):
         return [self.get_lr(iter_num, _base_lr) for _base_lr in self.base_lr]  # iters
 
     def set_init_lr(self, optimizer):
         for group in optimizer.param_groups:  # type: ignore
-            group.setdefault('initial_lr', group['lr'])
-            self.base_lr = [group['initial_lr'] for group in optimizer.param_groups  # type: ignore
-        ]
+            group.setdefault("initial_lr", group["lr"])
+            self.base_lr = [
+                group["initial_lr"] for group in optimizer.param_groups  # type: ignore
+            ]
 
 
 def train():
@@ -94,35 +90,40 @@ def train():
     arg = argp.parser.parse_args()
     arg_dict = vars(arg)
     if arg.arg_file is not None:
-        with open(arg.arg_file, 'rt') as f:
+        with open(arg.arg_file, "rt") as f:
             arg_dict.update(json.load(f))
 
-    if not os.path.exists(arg_dict['save_path']):
-        os.makedirs(arg_dict['save_path'])
-    with open(os.path.join(arg_dict['save_path'],  'arg.json'), 'wt') as f:
-      json.dump(arg_dict, f, indent=4)
+    if not os.path.exists(arg_dict["save_path"]):
+        os.makedirs(arg_dict["save_path"])
+    with open(os.path.join(arg_dict["save_path"], "arg.json"), "wt") as f:
+        json.dump(arg_dict, f, indent=4)
 
-    arg_dict['ann_file'] = arg_dict['ann_file_train']
-    arg_dict['test_mode'] = False 
+    arg_dict["ann_file"] = arg_dict["ann_file_train"]
+    arg_dict["test_mode"] = False
 
-    print('===> Loading datasets')
+    print("===> Loading datasets")
     # Initialize dataset
     dataset = build_dataset(arg_dict)
 
-    print('===> Building model')
+    print("===> Building model")
     # Initialize model parameters
     model = build_model(arg_dict)
-    if not arg_dict['cpu']:
+    if not arg_dict["cpu"]:
         model = model.cuda()
-    
+
     # Build loss
     loss = build_loss(arg_dict)
 
     # Build Optimzer
-    optimizer = optim.AdamW(model.parameters(), lr=arg_dict['lr'],  betas=(0.9, 0.999), weight_decay=arg_dict['weight_decay'])
+    optimizer = optim.AdamW(
+        model.parameters(),
+        lr=arg_dict["lr"],
+        betas=(0.9, 0.999),
+        weight_decay=arg_dict["weight_decay"],
+    )
 
     # Build lr scheduler
-    cosine_lr = CosineRestartLr(arg_dict['lr'], [arg_dict['max_iters']], [1], 1e-7)
+    cosine_lr = CosineRestartLr(arg_dict["lr"], [arg_dict["max_iters"]], [1], 1e-7)
     cosine_lr.set_init_lr(optimizer)
 
     epoch_loss = 0
@@ -130,10 +131,10 @@ def train():
     print_freq = 100
     save_freq = 10000
 
-    while iter_num < arg_dict['max_iters']:
+    while iter_num < arg_dict["max_iters"]:
         with tqdm(total=print_freq) as bar:
-            for feature, label, _ in dataset:        
-                if arg_dict['cpu']:
+            for feature, label, _ in dataset:
+                if arg_dict["cpu"]:
                     input, target = feature, label
                 else:
                     input, target = feature.cuda(), label.cuda()
@@ -151,17 +152,25 @@ def train():
                 optimizer.step()
 
                 iter_num += 1
-                
+
                 bar.update(1)
 
                 if iter_num % print_freq == 0:
                     break
 
-        print("===> Iters[{}]({}/{}): Loss: {:.4f}".format(iter_num, iter_num, arg_dict['max_iters'], epoch_loss / print_freq))
-        if iter_num % save_freq == 0:
-            checkpoint(model, iter_num, arg_dict['save_path'])
-        epoch_loss = 0
+        log_message = "===> Iters[{}]({}/{}): Loss: {:.4f}".format(
+            iter_num, iter_num, arg_dict["max_iters"], epoch_loss / print_freq
+        )
+        print(log_message)
 
+        log_file_path = Path("training_log.txt")
+        log_file_path.touch(exist_ok=True)
+        with log_file_path.open("a") as log_file:
+            log_file.write(log_message + "\n")
+
+        if iter_num % save_freq == 0:
+            checkpoint(model, iter_num, arg_dict["save_path"])
+        epoch_loss = 0
 
 
 if __name__ == "__main__":
